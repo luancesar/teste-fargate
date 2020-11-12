1c43ebc14c65d3d98e3a19cede528b2a1b012ce2
Luan Cesar <luan.cesar.sn@gmail.com>
2020-11-12 08:43:59 -0300

    Update index.ts

--- src/database/index.ts
+++ src/database/index.ts
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 require('dotenv/config');
 
 mongoose.connect(
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-w8s7a.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+  `mongodb+srv://teste:12345678@docdb-2020-11-12-11-38-46.cluster-cw08dk3wthpc.us-east-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false`,
   {
     useUnifiedTopology: true,
     useNewUrlParser: true,
